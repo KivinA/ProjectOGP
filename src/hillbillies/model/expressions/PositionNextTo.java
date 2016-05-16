f4cfdbe837405450cf6f77ea05719b615bc3bf63
Jeroen-D <frank@iMac-van-Frank.local>
2016-05-16 17:23:55 +0200

    .

--- src/hillbillies/model/expressions/PositionNextTo.java
+++ src/hillbillies/model/expressions/PositionNextTo.java
@@ -1,5 +1,6 @@
 package hillbillies.model.expressions;
 
+import java.util.Random;
 import hillbillies.model.Task;
 import hillbillies.part3.programs.SourceLocation;
 
@@ -23,10 +24,29 @@ public class PositionNextTo extends PositionExpression {
 		int[] coordinates = {coord[0], coord[1], coord[2]};
 		Integer[] neighbour = {coord[0], coord[1], coord[2]};
 		
-		if(coordinates[0] == task.getUnit().getWorld().getNbCubesX())
+		/*if(coordinates[0] == task.getUnit().getWorld().getNbCubesX())
 			neighbour[0] -= 1;
 		else
 			neighbour[0] += 1;
+		
+		return neighbour;*/
+		
+		Random random = new Random();
+		
+		while (true) {
+			int randomValue1 = random.nextInt(3) - 1;
+			int randomValue2 = random.nextInt(3) - 1;
+			int randomValue3 = random.nextInt(3) - 1;
+			
+			if(coordinates[0] != task.getUnit().getWorld().getNbCubesX() && coordinates[1] != task.getUnit().getWorld().getNbCubesY() && coordinates[2] != task.getUnit().getWorld().getNbCubesZ()) {
+				neighbour[0] += randomValue1;
+				neighbour[1] += randomValue2;
+				neighbour[2] += randomValue3;
+			}
+			
+			if (neighbour != coord)
+				break;
+		}
 		
 		return neighbour;
 	}
