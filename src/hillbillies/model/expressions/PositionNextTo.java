c0fa9524fa18c5b28ce8beaecfd7ee1e496aef3a
KivinA <kevin.algoet@gmail.com>
2016-05-20 18:59:50 +0200

    Fixed positionNextTo
    This caused some problems where Tasks woulmd dissapear because the
    calculated coordinates weren't passable. Although this might have fixed
    most of the problems with Tasks dissapearing, some Tasks might still
    dissapear even though they are executable.

--- src/hillbillies/model/expressions/PositionNextTo.java
+++ src/hillbillies/model/expressions/PositionNextTo.java
@@ -3,7 +3,7 @@ package hillbillies.model.expressions;
 import java.util.Arrays;
 import java.util.Random;
 import hillbillies.model.Task;
-import hillbillies.model.World;
+import hillbillies.model.Unit;
 
 /**
  * @author Kevin Algoet & Jeroen Depuydt
@@ -20,17 +20,20 @@ public class PositionNextTo<T extends PositionExpression> extends PositionExpres
 	@Override
 	public Integer[] evaluate(Task task) {
 		Integer[] coord = positionE.evaluate(task);
-		Integer[] neighbour = Arrays.copyOf(coord, coord.length);
-		World world = task.getUnit().getWorld();
-		
+		int[] neighbour = {coord[0], coord[1], coord[2]};
+		Integer[] neighbourInt = {neighbour[0], neighbour[1], neighbour[2]};
+		Unit unit = task.getUnit();
 		while (true) 
 		{
 			for (int i = 0; i < neighbour.length; i++)
+			{
 				neighbour[i] += (new Random().nextInt(3) - 1);
-			if (world.isBetweenBoundaries(neighbour[0], neighbour[1], neighbour[2]) && ! (Arrays.equals(neighbour, coord)))
+				neighbourInt[i] = neighbour[i];
+			}
+			if ( unit.canHaveAsCubeCoordinates(neighbour) && ! (Arrays.equals(neighbourInt, coord)))
 				break;
 		}
-		return neighbour;
+		return neighbourInt;
 	}
 
 }
